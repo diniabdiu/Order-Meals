75230a438bfede3737706ea69edf3fa67fd92645
Dini Abdiu <diniabdiu@gmail.com>
2022-04-18 21:55:50 +0200

    layout

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,51 @@
+import { useContext, useEffect, useState } from "react";
+
+// Components
+import CartIcon from "../Cart/CartIcon";
+
+// Context
+import CartContext from "../../store/cart-context";
+
+// Style
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  console.log(cartCtx, "her cartCtx");
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    console.log("HERE IS THE ITEM > AMOUNT ----> ", item);
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items, cartCtx.items.length]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,39 @@
+// Context
+import CartContext from "../../../store/cart-context";
+
+// Components
+import MealItemForm from "./MealItemForm";
+
+// Style
+import classes from "./MealItem.module.css";
+import { useContext } from "react";
+
+const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} id={props.id} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
